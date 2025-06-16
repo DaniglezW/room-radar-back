157c11c71d1c882af179d36b1e26c65c6773bf6e
DaniglezW <dani.flex.work@gmail.com>
2025-06-16 08:36:58 +0100

    Feat :zap:: Added CorsConfig V3

--- src/main/java/com/practice/server/application/config/CorsConfig.java
+++ src/main/java/com/practice/server/application/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://roomradar-web.vercel.app/app")
+                .allowedOrigins("http://localhost:3000", "https://roomradar-web.vercel.app/app", "https://roomradar-web.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
